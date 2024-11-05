8efae0906c1b13e8268bb552203006528e9a54ea
benedikt <benedikt.reich05@gmail.com>
2024-11-05 10:45:37 +0100

    Added basic site with placeholder text

---
+++ src/components/Programs/Programs.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Programs: React.FC = () => {
+    return (
+        <div className="p-8">
+            <h2 className="text-3xl font-bold">Our Programs</h2>
+            <p className="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at aut autem commodi culpa ea earum hic natus, perspiciatis sunt. A aliquid aperiam corporis distinctio ex officiis perspiciatis rerum tempora?</p>
+            <img src="https://via.placeholder.com/600x400" alt="Placeholder" className="mt-8" />
+        </div>
+    );
+};
+
+export default Programs;
