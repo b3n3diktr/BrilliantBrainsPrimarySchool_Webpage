3515ff5b502469cdb0393a7d678ca52a988d5dec
benedikt <benedikt.reich05@gmail.com>
2024-11-05 10:42:34 +0100

    Added basic sites with placeholder text

---
+++ src/components/About/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="p-8">
+            <h2 className="text-3xl font-bold">About</h2>
+            <p className="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate facere molestiae quis veritatis. Exercitationem facere ipsa nobis quod repellat sequi sunt. Culpa expedita iure iusto minima omnis ratione sit tempore!</p>
+            <img src="https://via.placeholder.com/600x400" alt="Placeholder" className="mt-8" />
+        </div>
+    );
+};
+
+export default About;
