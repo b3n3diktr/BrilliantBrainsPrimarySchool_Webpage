8d30c2856101994416910595c25f8cd42fadd0d2
benedikt <benedikt.reich05@gmail.com>
2024-11-09 22:26:51 +0100

    Changed styling of the staff page and added some text

--- src/components/About/Staff/Staff.tsx
+++ src/components/About/Staff/Staff.tsx
@@ -20,89 +20,77 @@ const Staff: React.FC = () => {
 
             <div
                 className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 xl:grid-cols-4 mx-auto max-w-7xl p-10 ">
-                <div className="border-2 shadow p-4 text-center rounded-lg flex flex-col  items-center">
-                    <img src={Boys_Patron_Michael} alt="Placeholder" className="mt-8"/>
-                    <div className="p-4">
-                        <a> Boys Patron Michael </a>
-                    </div>
+                <div className="border shadow p-4 text-center rounded-xl flex flex-col  items-center"><img
+                    src="/images/Boys_Patron_Michaelc08bdbcab6bf486413d4.png" alt="Placeholder"
+                    className="rounded-lg shadow-mt"/>
+                    <div className="p-4"><a> Boys Patron Michael </a></div>
                 </div>
-                <div className="border-2 shadow p-4 text-center rounded-lg flex flex-col  items-center">
-                    <img src={CoDirector_Esther} alt="Placeholder" className="mt-8"/>
-                    <div className="p-4">
-                        <a> Co-Director Esther </a>
-                    </div>
+                <div className="border shadow p-4 text-center rounded-xl flex flex-col  items-center"><img
+                    src="/images/Co-Director_Estherb2d4ae93a1274e4ade39.png" alt="Placeholder"
+                    className="rounded-lg shadow-mt"/>
+                    <div className="p-4"><a> Co-Director Esther </a></div>
                 </div>
-                <div className="border-2 shadow p-4 text-center rounded-lg flex flex-col  items-center">
-                    <img src={Cook_Robert} alt="Placeholder" className="mt-8"/>
-                    <div className="p-4">
-                        <a> Cook Robert </a>
-                    </div>
+                <div className="border shadow p-4 text-center rounded-xl flex flex-col  items-center"><img
+                    src="/images/Cook_Robertbaeac193bc418f1e13ed.png" alt="Placeholder"
+                    className="rounded-lg shadow-mt"/>
+                    <div className="p-4"><a> Cook Robert </a></div>
                 </div>
-                <div className="border-2 shadow p-4 text-center rounded-lg flex flex-col  items-center">
-                    <img src={Cooking_Chef_Grandma} alt="Placeholder" className="mt-8"/>
-                    <div className="p-4">
-                        <a> Cooking Chef Grandma </a>
-                    </div>
+                <div className="border shadow p-4 text-center rounded-xl flex flex-col  items-center"><img
+                    src="/images/Cooking_Chef_Grandmaac52d08a60e45770c46b.png" alt="Placeholder"
+                    className="rounded-lg shadow-mt"/>
+                    <div className="p-4"><a> Cooking Chef Grandma </a></div>
                 </div>
-                <div className="border-2 shadow p-4 text-center rounded-lg flex flex-col  items-center">
-                    <img src={Director_Gideon} alt="Placeholder" className="mt-8"/>
-                    <div className="p-4">
-                        <a> Director Gideon </a>
-                    </div>
+                <div className="border shadow p-4 text-center rounded-xl flex flex-col  items-center"><img
+                    src="/images/Director_Gideonedeceaa60bb54993e29d.png" alt="Placeholder"
+                    className="rounded-lg shadow-mt"/>
+                    <div className="p-4"><a> Director Gideon </a></div>
                 </div>
-                <div className="border-2 shadow p-4 text-center rounded-lg flex flex-col  items-center">
-                    <img src={Driver_Albert} alt="Placeholder" className="mt-8"/>
-                    <div className="p-4">
-                        <a> Driver Albert </a>
-                    </div>
+                <div className="border shadow p-4 text-center rounded-xl flex flex-col  items-center"><img
+                    src="/images/Driver_Albertd71b9d340ab07f81ed71.png" alt="Placeholder"
+                    className="rounded-lg shadow-mt"/>
+                    <div className="p-4"><a> Driver Albert </a></div>
                 </div>
-                <div className="border-2 shadow p-4 text-center rounded-lg flex flex-col  items-center">
-                    <img src={Headteacher_Daniel} alt="Placeholder" className="mt-8"/>
-                    <div className="p-4">
-                        <a> Headteacher Daniel </a>
-                    </div>
+                <div className="border shadow p-4 text-center rounded-xl flex flex-col  items-center"><img
+                    src="/images/Headteacher_Daniela2b90c43e2ec04c7b804.png" alt="Placeholder"
+                    className="rounded-lg shadow-mt"/>
+                    <div className="p-4"><a> Headteacher Daniel </a></div>
                 </div>
-                <div className="border-2 shadow p-4 text-center rounded-lg flex flex-col  items-center">
-                    <img src={School_Doctor_Susan} alt="Placeholder" className="mt-8"/>
-                    <div className="p-4">
-                        <a> School Doctor Susan </a>
-                    </div>
+                <div className="border shadow p-4 text-center rounded-xl flex flex-col  items-center"><img
+                    src="/images/School_Doctor_Susan35882ea31487d4e2cc61.png" alt="Placeholder"
+                    className="rounded-lg shadow-mt"/>
+                    <div className="p-4"><a> School Doctor Susan </a></div>
                 </div>
-                <div className="border-2 shadow p-4 text-center rounded-lg flex flex-col  items-center">
-                    <img src={Teacher_Gloria} alt="Placeholder" className="mt-8"/>
-                    <div className="p-4">
-                        <a> Teacher Gloria </a>
-                    </div>
+                <div className="border shadow p-4 text-center rounded-xl flex flex-col  items-center"><img
+                    src="/images/Teacher_Gloriab7cbc5d09a0663e61153.png" alt="Placeholder"
+                    className="rounded-lg shadow-mt"/>
+                    <div className="p-4"><a> Teacher Gloria </a></div>
                 </div>
-                <div className="border-2 shadow p-4 text-center rounded-lg flex flex-col  items-center">
-                    <img src={Tr_Ben} alt="Placeholder" className="mt-8"/>
-                    <div className="p-4">
-                        <a> Tr. Ben </a>
-                    </div>
+                <div className="border shadow p-4 text-center rounded-xl flex flex-col  items-center"><img
+                    src="/images/Tr._Benb94e5361e296a9de5f9e.png" alt="Placeholder" className="rounded-lg shadow-mt"/>
+                    <div className="p-4"><h3 className="text-lg font-semibold text-gray-800"> Tr. Ben</h3></div>
+                    <div className="text-gray-600 italic"><p>“I chose Brilliant Brains because I found it is a good
+                        project where I can achieve my dreams!”</p></div>
                 </div>
-                <div className="border-2 shadow p-4 text-center rounded-lg flex flex-col  items-center">
-                    <img src={Tr_Francis} alt="Placeholder" className="mt-8"/>
-                    <div className="p-4">
-                        <a> Tr. Francis </a>
-                    </div>
+                <div className="border shadow p-4 text-center rounded-xl flex flex-col  items-center"><img
+                    src="/images/Tr._Francis8be39b975b2bbcf63cf9.png" alt="Placeholder"
+                    className="rounded-lg shadow-mt"/>
+                    <div className="p-4"><a> Tr. Francis </a></div>
                 </div>
-                <div className="border-2 shadow p-4 text-center rounded-lg flex flex-col  items-center">
-                    <img src={Tr_Janet} alt="Placeholder" className="mt-8"/>
-                    <div className="p-4">
-                        <a> Tr. Janet </a>
-                    </div>
+                <div className="border shadow p-4 text-center rounded-xl flex flex-col  items-center"><img
+                    src="/images/Tr._Janet50e67ac37fc8beb423bf.png" alt="Placeholder" className="rounded-lg shadow-mt"/>
+                    <div className="p-4"><a> Tr. Janet </a></div>
+                    <div className="text-gray-600 italic"><p>“I chose Brilliant Brains because I support the project and
+                        I wanted to impact knowledge to the vulnerable children of Brilliant Brains“</p></div>
                 </div>
-                <div className="border-2 shadow p-4 text-center rounded-lg flex flex-col  items-center">
-                    <img src={Tr_Patience} alt="Placeholder" className="mt-8"/>
-                    <div className="p-4">
-                        <a> Tr. Patience </a>
-                    </div>
+                <div className="border shadow p-4 text-center rounded-xl flex flex-col  items-center"><img
+                    src="/images/Tr._Patienceb5a5b065dbf30c9cc404.png" alt="Placeholder"
+                    className="rounded-lg shadow-mt"/>
+                    <div className="p-4"><a> Tr. Patience </a></div>
                 </div>
-                <div className="border-2 shadow p-4 text-center rounded-lg flex flex-col  items-center">
-                    <img src={Tr_Stellah} alt="Placeholder" className="mt-8"/>
-                    <div className="p-4">
-                        <a> Tr. Stellah </a>
-                    </div>
+                <div className="border shadow p-4 text-center rounded-xl flex flex-col  items-center"><img
+                    src="/images/Tr._Stellahce7ed56e23f47b188908.png" alt="Placeholder"
+                    className="rounded-lg shadow-mt"/>
+                    <div className="p-4"><a> Tr. Stellah </a></div>
                 </div>
             </div>
 
